e6c0b3966c433dcb9e035a2946157e2845f70baa
Jonathan <jonigl@gmail.com>
2024-12-11 16:03:20 +0100

    [main] Adding the most basic flex layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
-import './App.css'
+import "./App.css";
 
 export default function App() {
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
-  )
+    <div className="flex h-screen flex-col">
+      <div className="bg-red-500">header</div>
+      <div className="grow bg-green-500">content</div>
+      <div className="bg-blue-500">footer</div>
+    </div>
+  );
 }
